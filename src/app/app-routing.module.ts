6323bf348c678e77eb763b20e8d79ba5b7ff3f2c
rawat10deepak <109275183+rawat10deepak@users.noreply.github.com>
2023-01-09 15:50:55 +0530

    location stratergy - use hash

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -57,6 +57,7 @@ const appRoutes: Routes = [
 ];
 
 @NgModule({
+  // imports: [RouterModule.forRoot(appRoutes, { useHash: true })],
   imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule],
 })
